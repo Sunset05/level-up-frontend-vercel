7e6b7875f7e8cea20b5d89a0dde3bcbd1b169633
Keith Funk <Keith.C.Funk@gmail.com>
2021-09-01 21:26:38 -0600

    changed firebase API to depend on process. tracked firebase.js

---
+++ src/firebase/firebase.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app";
+import "firebase/storage";
+
+var firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
+
+const storage = firebase.storage();
+
+export { storage, firebase as default };
