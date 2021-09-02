3b10a5dfd5254e7656cc6a71fc6b4a00d27a3290
Keith Funk <Keith.C.Funk@gmail.com>
2021-09-01 21:42:08 -0600

    formatted code with prettier, fixing console warnings

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,26 @@
-import React from "react"
-import { Link } from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Header() {
-    return (
-        <div className="header">
-            <div className="inner-header">
-                <div className="logo-container"></div>
+  return (
+    <div className="header">
+      <div className="inner-header">
+        <div className="logo-container"></div>
 
-                <ul className="navigation">            
-                    <Link to="/">
-                        <li className="navbar-link">Profile</li>
-                    </Link>
+        <ul className="navigation">
+          <Link to="/">
+            <li className="navbar-link">Profile</li>
+          </Link>
 
-                    <Link to="/trade/">
-                        <li className="navbar-link">Trade</li>
-                    </Link>
+          <Link to="/trade/">
+            <li className="navbar-link">Trade</li>
+          </Link>
 
-                    <Link to="/signup">
-                        <li className="navbar-link">Logout</li>
-                    </Link>
-                </ul>
-            </div>
-        </div>
-    )
+          <Link to="/signup">
+            <li className="navbar-link">Logout</li>
+          </Link>
+        </ul>
+      </div>
+    </div>
+  );
 }
--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,23 +1,22 @@
-import React, { useState, useEffect } from "react"
-import DirectMessages from "./DirectMessages"
+import React from "react";
+import DirectMessages from "./DirectMessages";
 
 export default function Messages({ match, location, user, addNewMessage }) {
+  return (
+    <div className="message-page-container">
+      <div className="message-sender-container">
+        <h1>chatting with {match.params.author}</h1>
+      </div>
 
-    return (
-        <div className="message-page-container">
-            <div className="message-sender-container">
-                <h1>chatting with { match.params.author }</h1>
-            </div>
-
-            <div className="direct-message-container">
-                <DirectMessages 
-                    chattingWithId={ location.state.authorId }
-                    user={ user }
-                    sentMessages= { user.sent_messages } 
-                    receivedMessages={ user.received_messages }
-                    addNewMessage= { addNewMessage }
-                />
-            </div>
-        </div> 
-    )
+      <div className="direct-message-container">
+        <DirectMessages
+          chattingWithId={location.state.authorId}
+          user={user}
+          sentMessages={user.sent_messages}
+          receivedMessages={user.received_messages}
+          addNewMessage={addNewMessage}
+        />
+      </div>
+    </div>
+  );
 }
